135dbe61dc3a104e302dfb0feb0a7703fc9709a7
skorlypka <137394530+skorlypka@users.noreply.github.com>
2023-10-31 12:31:26 +0200

    +

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,7 @@ document.querySelector('body').style.backgroundColor = '#eaf1fad0';
 const inputRangeRef = document.getElementById('font-size-control');
 const textRef = document.getElementById('text');
 
-function fontSizeControl(event) {
-    textRef.style.fontSize = event.currentTarget.value + 'px';
-}
+const fontSizeControl = ({ currentTarget }) =>
+    (textRef.style.fontSize = `${currentTarget.value}px`);
+
+inputRangeRef.addEventListener('input', fontSizeControl);
